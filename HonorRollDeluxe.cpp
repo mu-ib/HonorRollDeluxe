2305c2b41b4639028a2c5e3d2b9e94a7f0e0f9e9
d4-rk <ib.muhammad725@gmail.com>
2021-06-02 21:02:12 -0400

    Copied Code over from honor roll 1

--- HonorRollDeluxe.cpp
+++ HonorRollDeluxe.cpp
@@ -1,11 +1,51 @@
-// HonorRollDeluxe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/***
+* Muhammad Ibrahim
+* 9 February 2021
+* Honor Roll Procedural
+***/
 
-#include <iostream>
+#include "Functions.h";
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	vector <string> courseNames;
+	vector <int> courseGrades;
+
+
+	//Input
+	//student's full name
+	string name;
+	cout << "Enter your full name: ";
+	getline(cin, name);
+
+	//get the courses information
+	int numCourses = getNumCourses();
+	courseNames = getCourseNames(courseNames, numCourses);
+	courseGrades = getCourseGrades(courseGrades, courseNames);
+
+	int avg = findAverage(courseGrades);
+
+	//output
+	cout << endl << endl << endl
+		<< name << endl << endl
+		<< setw(0) << "Class" << setw(27) << "Grade" << endl;
+	for (int i = 0; i < courseNames.size(); i++) {
+		cout << setw(0) << courseNames[i] << setw(32 - courseNames[i].length()) << courseGrades[i] << endl;
+	}
+	cout << "Average: " << avg << endl
+		<< "Disciplinary Infraction: ";
+	bool b = disciplineIssue();
+	if (b)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+	//determine if student is eligible
+	if (isHonorRoll(courseGrades, avg, b)) {
+		cout << "Congratulations, " << name << ", you have made the honor roll." << endl << endl;
+	}
+	else
+		cout << "I'm sorry, " << name << ", but you didn't qualify for the honor roll." << endl << endl;
 }
